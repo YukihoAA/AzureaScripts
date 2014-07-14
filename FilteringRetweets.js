6ea45140222528880d0f345b0c5c59272ccb9ee7
시나마후유 <SinaMafuyu@users.noreply.github.com>
2014-07-14 21:10:53 +0900

    Update FilteringRetweets.js

--- FilteringRetweets.js
+++ FilteringRetweets.js
@@ -6,7 +6,7 @@ var modify = eval(System.settings.getValue('user.Fillter', 'modify'));
 if (modify == 0 | !modify)
 	System.showNotice('모든 리트윗을 필터링하지 않습니다');
 else if (modify == 1)
-	System.showNotice('수동 리트윗을 필터링합니다. (QT, RT로 시작하는 트윗)');
+	System.showNotice('QT, RT로 시작하는 수동 리트윗을 필터링합니다.');
 else if (modify == 2)
 	System.showNotice('모든 수동 리트윗을 필터링합니다.');
 else if (modify == 3)
@@ -64,7 +64,7 @@ System.addKeyBindingHandler('Q'.charCodeAt(0), 1,
 		{
 			System.settings.setValue('user.Fillter', 'modify', 1);
 			System.settings.reconfigure();
-			System.showNotice('수동 리트윗을 필터링합니다 (QT, RT로 시작하는 트윗)');
+			System.showNotice('QT, RT로 시작하는 수동 리트윗을 필터링합니다.');
 			modify = 1;
 		}
 		else if (modify == 1)
